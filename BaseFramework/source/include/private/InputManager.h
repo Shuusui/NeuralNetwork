47d8191ec085cde41442da0a713745e6c8bcce6a
Unknown <K.schaefer92@gmx.de>
2017-11-09 12:12:25 +0100

    Created the base framework of the neural Network,
    I began to create the base framework of the neural Netowrk and implemented some classes

---
+++ BaseFramework/source/include/private/InputManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+///Internal includes
+#pragma region Internal Includes
+
+#pragma endregion Internal Includes
+///External includes
+#pragma region External Includes
+#include <string>
+#pragma region External Includes
+
+namespace NeuralNetwork
+{
+	namespace BaseFramework
+	{
+		class InputManager
+		{
+		public: 
+			InputManager();
+			~InputManager();
+		private: 
+			
+		};
+
+
+	}
+}
